8675c3fbc7c416baf9574c8d9558e1748531bfe0
Carles Far Pierres <carlesfarpierres@MacBook-Pro-de-Carles.local>
2023-08-03 22:49:31 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const app = require('./app');
+const http = require("http"); 
+
 const port = process.env.PORT || 4000;
+let server = http.createServer(app);
+
 app.get('/', function(req, res) {
   res.sendfile('public/index.html');
 });
